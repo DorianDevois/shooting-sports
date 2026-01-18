edd130edb556f7884cb3dc79ed96e079a99c498e
Dorian Devois <dorian.devois@gmail.com>
2026-01-18 17:35:36 +0400

    add some changes

--- js/modal-templates.js
+++ js/modal-templates.js
@@ -70,11 +70,11 @@ export function callbackTemplate() {
         <!-- Callback Form -->
         <div class="callback-form">
           <h2 class="callback-form__title">Feedback Request</h2>
-          <p class="callback-form__description">Leave your details and we'll contact you back</p>
+          <p class="callback-form__call-to-action">Leave your details and we'll contact you back</p>
 
           <form class="js-speaker-form callback-form__form" autocomplete="off" data-form="callback">
             <!-- Name Field -->
-            <label class="callback-form__field callback-form__field--input">
+            <label class="callback-form__field callback-form__field--floating-label">
               <input class="callback-form__input" type="text" name="user-name" placeholder=" " required />
               <span class="callback-form__label">Name</span>
               <svg class="callback-form__icon" width="16px" height="16px">
@@ -83,7 +83,7 @@ export function callbackTemplate() {
             </label>
 
             <!-- Phone Field -->
-            <label class="callback-form__field callback-form__field--input">
+            <label class="callback-form__field callback-form__field--floating-label">
               <input class="callback-form__input" type="tel" name="user-phone" placeholder=" " required />
               <span class="callback-form__label">Phone</span>
               <svg class="callback-form__icon" width="16px" height="16px">
@@ -92,7 +92,7 @@ export function callbackTemplate() {
             </label>
 
             <!-- Email Field -->
-            <label class="callback-form__field callback-form__field--input">
+            <label class="callback-form__field callback-form__field--floating-label">
               <input class="callback-form__input" type="email" name="user-email" placeholder=" " required />
               <span class="callback-form__label">Email</span>
               <svg class="callback-form__icon" width="16px" height="16px">
@@ -101,13 +101,13 @@ export function callbackTemplate() {
             </label>
 
             <!-- Message Field -->
-            <label class="callback-form__field callback-form__field--textarea">
+            <label class="callback-form__field callback-form__field--stacked-label">
               <span class="callback-form__label">Comment</span>
               <textarea class="callback-form__textarea" name="user-comment" placeholder="Message"></textarea>
             </label>
 
             <!-- Policy Checkbox -->
-            <label class="callback-form__field callback-form__field--checkbox">
+            <label class="callback-form__field callback-form__field--inline-label">
               <input class="callback-form__checkbox" type="checkbox" name="user-policy" required />
               <span class="callback-form__checkbox-custom"></span>
               <span class="callback-form__checkbox-label">
