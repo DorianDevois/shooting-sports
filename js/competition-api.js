9ab9d762ae16b2d577c0b355e1f6f130ba56905a
Dorian Devois <dorian.devois@gmail.com>
2026-01-31 11:41:38 +0400

    fix js file path

--- js/competition-api.js
+++ js/competition-api.js
@@ -1,4 +1,4 @@
 export const fetchCompetitions = async () => {
-  const response = await fetch('/data/generated/competitions.json');
+  const response = await fetch('data/generated/competitions.json');
   return response.json();
 };
